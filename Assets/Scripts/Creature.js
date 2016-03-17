971b29e06e282e86d0bdca6f4ae0d4b3fae1da20
ssnake <max.nedelchev@gmail.com>
2016-03-17 15:57:30 +0200

    moving player

---
+++ Assets/Scripts/Creature.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+public class Creature extends MonoBehaviour {
+    
+    var direction = new Vector2();
+    var leftKey = 'left1';
+    var rightKey = 'right1';
+    var upKey = 'up1';
+    var downKey = 'down1';
+    
+    var speed : float = 0.04;
+
+    function Start () {
+        
+    }
+
+    function Update () {
+        GetKey();
+    }
+    private function GetKey() {
+        if (Input.GetButtonDown(leftKey) ) {
+            this.direction = Vector2.left;
+            Debug.Log('Left');
+        } else if (Input.GetButtonDown(rightKey)) {
+            this.direction = Vector2.right;
+            Debug.Log('Right');
+        } else if (Input.GetButtonDown(upKey)) {
+            this.direction = Vector2.up;
+        } else if (Input.GetButtonDown(downKey)) {
+            this.direction = Vector2.down;
+        }
+        transform.position += this.direction * speed;
+        //Debug.Log('direct: ' + this.speed);
+    }
+}
--- Assets/Scripts/control.js
+++ Assets/Scripts/control.js
@@ -5,5 +5,10 @@ function Start () {
 }
 
 function Update () {
+    var left1 = Input.GetButton('left1');
+    var right1 = Input.GetButton('right1');
+    Debug.Log("left1" + left1);
+    Debug.Log("right1" + right1);
+
 
 }
